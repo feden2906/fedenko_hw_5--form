85e8b597ea4ffaf797eee52a4c72f3edf2acaeb2
Fedenko Maksym <feden2906@gmail.com>
2020-12-15 12:27:07 +0200

    zagotovka

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from "./components/header/Header";
 import {BrowserRouter as Router} from "react-router-dom";
+import Body from "./components/body/Body";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header/>
-
+        <Body/>
       </Router>
     </div>
   );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import './Users.css'
 import TakeDataFromAPI from "../../services/TakeDataFromAPI";
 
+
 class Users extends Component {
   state = {users: null, chosenUser: null}
 
 
   render() {
     return (
-        <div>
-
+        <div className='users-wrapper'>
+          <h2 className='text-users'>Users</h2>
+          <div className='all-block'>
+            <FilterForm/>
+          </div>
         </div>
     )
   }
