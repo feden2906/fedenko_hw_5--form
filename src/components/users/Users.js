9957dd29ee258b9977c27c7afe512d8a23bf3235
Fedenko Maksym <feden2906@gmail.com>
2020-12-15 12:33:43 +0200

    Revert "zagotovka"
    This reverts commit 85e8b597ea4ffaf797eee52a4c72f3edf2acaeb2.

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,18 +2,14 @@ import React, {Component} from 'react';
 import './Users.css'
 import TakeDataFromAPI from "../../services/TakeDataFromAPI";
 
-
 class Users extends Component {
   state = {users: null, chosenUser: null}
 
 
   render() {
     return (
-        <div className='users-wrapper'>
-          <h2 className='text-users'>Users</h2>
-          <div className='all-block'>
-            <FilterForm/>
-          </div>
+        <div>
+
         </div>
     )
   }
